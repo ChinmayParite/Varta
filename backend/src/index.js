4e3d55c22fc607bb667d73a8ef2117e650f72dd1
dAddyP001 <134932493+ChinmayParite@users.noreply.github.com>
2024-12-26 18:44:48 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -15,7 +15,8 @@ const PORT = process.env.PORT || 5001
 const __dirname = path.resolve();
 
 // Middleware configuration
-app.use(express.json());
+app.use(express.json({ limit: '50mb' })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: '50mb', extended: true })); // Increase URL-encoded payload limit
 app.use(cookieParser());
 app.use(
   cors({
@@ -23,6 +24,7 @@ app.use(
     credentials: true,
   })
 );
+
 
 // API routes
 app.use("/api/auth", authRoutes);
